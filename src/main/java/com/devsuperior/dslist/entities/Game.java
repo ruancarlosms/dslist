c5db169e53d865facc2a1b87b0422261108a0391
Ruan Carlos <ruancarlos2703@gmail.com>
2025-01-22 19:38:48 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -20,22 +20,28 @@ public class Game {
 	
 	@Column(name = "game_year") //customizando o nome da coluna no banco
 	private Integer year;
-	private String gente;
-	private String plataform;
+	private String genre;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 	}
 
-	public Game(Long id, String title, Integer year, String gente, String plataform, String imgUrl,
-			String shortDescription, String longDescription) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, 
+			String imgUrl, String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
-		this.gente = gente;
-		this.plataform = plataform;
+		this.genre = genre;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -65,20 +71,28 @@ public class Game {
 		this.year = year;
 	}
 
-	public String getGente() {
-		return gente;
+	public String getGenre() {
+		return genre;
 	}
 
-	public void setGente(String gente) {
-		this.gente = gente;
+	public void setGenre(String genre) {
+		this.genre = genre;
 	}
 
-	public String getPlataform() {
-		return plataform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlataform(String plataform) {
-		this.plataform = plataform;
+	public void setPlataforms(String platforms) {
+		this.platforms = platforms;
+	}
+	
+	public Double getScore() {
+		return score;
+	}
+	
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
